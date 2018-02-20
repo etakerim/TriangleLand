cce91a6d95853941ea466a14991cf5b4a78199d6
Miroslav Hájek <mirkousko@gmail.com>
2018-02-20 20:59:28 +0100

    First approach in vertex creation

--- game.py
+++ game.py
@@ -12,6 +12,18 @@ winsize = (800, 600)
 pygame.init()
 canvas = pygame.display.set_mode(winsize)
 
+def rhombus(x_inc):
+    r = 5
+    a = [20, winsize[1] // 2]
+
+    for i in range(1, 9):
+        for y in range(8):
+            pygame.draw.ellipse(canvas, BLUE, (a[0] - r, a[1] - r, 2*r, 2*r))
+            a = [a[0] + x_inc, a[1] - 25]
+        a = [20 + i * x_inc, (winsize[1] // 2) + i * 25]
+
+
+rhombus(50)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:

d28f375bd141f2e9753b6a6fd96955ca48dc2154
Miroslav Hájek <mirkousko@gmail.com>
2018-02-20 20:56:15 +0100

    Color constants

--- game.py
+++ game.py
@@ -1,6 +1,13 @@
 import sys
 import pygame
 
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
 winsize = (800, 600)
 pygame.init()
 canvas = pygame.display.set_mode(winsize)
